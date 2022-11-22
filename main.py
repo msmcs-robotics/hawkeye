a92087029f2a5eea013bb11bc7dd036238d2e870
gndpwnd <gnelsondev@gmail.com>
2022-11-21 20:28:24 -0900

    sorta multi-object support

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
+import math
 import cv2
 from time import sleep
 
-cap = cv2.VideoCapture(0)
 
-MAX_WIDTH = 500
-MAX_HEIGHT = 500
 
+# if objects are too close to each other, they are considered the same object 
+dist_btw_objs_thresh = 50
+
+# Create the background subtractor
 hist_size = 10
-subtractor_threshold = 90
+subtractor_threshold = 50
 subtractor_detect_shadows = False
-
-
 subtractor = cv2.createBackgroundSubtractorMOG2(history=hist_size, varThreshold=subtractor_threshold, detectShadows=subtractor_detect_shadows)
 
+# Camera settings
+cap = cv2.VideoCapture(0)
+time_between_frames = 0.01
+MAX_WIDTH = 500
+MAX_HEIGHT = 500
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, MAX_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, MAX_HEIGHT)
 
-
-def target_tuning(x, y, w, h):
-    #w += 10
-    #h += 10
-
-    # threshold for size of object?
-    if w <= 20 or h <= 20:
-        pass
-    else:
-        x, y, w, h = x, y, w, h
-        
-    return x, y, w, h
-
-while True:  
+def get_frame():
     _, frame = cap.read()
 
     new_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -50,24 +42,56 @@ while True:
 
     x = []
 
+    for contour in contours:
+        x.append(len(contour))
+    m = max(x)
+    p = [i for i, j in enumerate(x) if j == m]
+
+    x, y, w, h = cv2.boundingRect(contours[p[0]])
+
+    return x, y, w, h, frame
+
+def check_dist_thresh(x1, x2, y1, y2):
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    if dist < dist_btw_objs_thresh:
+        return False
+    else:
+        return True
+
+while True:  
+
     try:
-        for contour in contours:
-            x.append(len(contour))
-        m = max(x)
-        p = [i for i, j in enumerate(x) if j == m]
+        x1, y1, w1, h1, frame1 = get_frame()
+        sleep(time_between_frames)
+        x2, y2, w2, h2, frame2 = get_frame()
 
-        x, y, w, h = cv2.boundingRect(contours[p[0]])
+        # if object is not large enough, skip
+        if w1 < 50 or h1 < 50:
+            obj1 = False
+        else:
+            obj1 = True
 
-        x, y, w, h = target_tuning(x, y, w, h)
-        
-        color = (0, 0, 255)
+        if w2 < 50 or h2 < 50:
+            obj2 = False
+        else:
+            obj2 = True
 
-        cv2.rectangle(frame, (x,y),(x+w,y+h),color, 3)
-        cv2.imshow("result", frame)
+        if obj1 and obj2:
+            if check_dist_thresh(x1, x2, y1, y2):
+                print("multiple objects")
+                cv2.rectangle(frame1, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+                cv2.rectangle(frame2, (x2, y2), (x2 + w2, y2 + h2), (255, 0, 0), 2)
+            else:
+                print("same object")
+                cv2.rectangle(frame1, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+        else:
+            print("no objects (rel)")
+
+        cv2.imshow("frame1", frame1)
+        cv2.imshow("frame2", frame2)
 
     except:
-        #print("Nothing Found...")
-        pass
+        print("no objects (abs)")
 
     key = cv2.waitKey(30)  
     if key == 27:  
