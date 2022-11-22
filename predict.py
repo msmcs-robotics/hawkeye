bf468d9e67e2969542fdccd62ec04bf1faae4e02
gndpwnd <gnelsondev@gmail.com>
2022-11-22 00:21:28 -0900

    init depth perception

--- predict.py
+++ predict.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2
+from colorama import Fore
 
+def fancy_depth_predict(x, y, face_num_index):
+    print(Fore.GREEN, 
+        "Face #",Fore.RED,face_num_index+1, Fore.GREEN, "Depth at:",
+        Fore.CYAN,
+        x,
+        Fore.WHITE,
+        ":",
+        Fore.YELLOW, 
+        y)
 class Predict_Centers:
 
     def __init__(self, widths1, widths2, heights1, heights2, x_centers1, y_centers1, x_centers2, y_centers2, max_width, max_height):
@@ -41,24 +51,56 @@ class Predict_Centers:
 
         return x, y
 
-    def depth_perception(self, x1, x2, y1, y2, w1, w2, h1, h2):
-        
-        ratio = (w1/w2)
+    def depth_perception(self, x, y, w1, w2, h1, h2):
 
+        delta_w = w2 - w1
+        delta_h = h2 - h1
+
+        # avg change in size
+        ratio = (delta_w + delta_h) / 2
+        ratio = round((ratio / 100), 2)
+
+        # if ratio is greater than 1, the object is getting closer
+        # if ratio is less than 1, the object is getting further away
+        # if ratio is equal to 1, the object is not moving
+
+        # therefore
+
+        # if ratio is greater than 1, we want to aim closer to the object
+        # if ratio is less than 1, we want to aim further away from the object
+        # if ratio is equal to 1, we want to aim at the same distance as the object
+        xneg = False
+        if x < 0:
+            xneg = True
+
+        # if aiming closer to the object, subtract value from x_center
         if ratio > 1:
-            ratio = -ratio
-
-        # the increase of the distance between the two points
-        delta_x = x2 - x1
-        delta_y = y2 - y1
-
-        # multiply the increase with the ratio
-        delta_x = delta_x * ratio
-        delta_y = delta_y * ratio
-
-        # add the increase to the second point
-        x3 = x2 + delta_x
-        y3 = y2 + delta_y
+            x3 = abs(x) - (abs(x) * ratio)
+        # if aiming further away from the object, add value to x_center
+        elif ratio < 1:
+            x3 = abs(x) + (abs(x) * ratio)
+        # if ratio is equal to 1, aim at the same distance as the object
+        else:
+            x3 = x
+        # check negative
+        if xneg == True:
+            x3 = x3 * -1
+        
+        yneg = False
+        if y < 0:
+            yneg = True
+        # if aiming closer to the object, subtract value from y_center
+        if ratio > 1:
+            y3 = abs(y) - (abs(y) * ratio)
+        # if aiming further away from the object, add value to y_center
+        if ratio < 1:
+            y3 = abs(y) + (abs(y) * ratio)
+        # if ratio is equal to 1, aim at the same distance as the object
+        else:
+            y3 = y
+        # check negative
+        if yneg == True:
+            y3 = y3 * -1
 
         x3, y3 = self.check_out_of_frame(x3, y3)
 
@@ -107,9 +149,13 @@ class Predict_Centers:
         for i in range(len(self.pos1_vecs)):
             self.calc_r3(self.pos1_vecs[i], self.pos2_vecs[i])
         
-        #for i in range(len(self.vec_centersX)):
-            #self.depth_perception(self.vec_centersX[i], self.dep_centersX[i], self.vec_centersY[i], self.dep_centersY[i], self.widths1[i], self.widths2[i], self.heights1[i], self.heights2[i])
+        #print("number of vec centers:" + str(len(self.vec_centersX)))
+
+        for i in range(len(self.vec_centersX)):
+            self.depth_perception(self.vec_centersX[i], self.vec_centersY[i], self.widths1[i], self.widths2[i], self.heights1[i], self.heights2[i])
+            fancy_depth_predict(self.dep_centersX[i], self.dep_centersY[i], i)
         
+        #print("number of dep centers:" + str(len(self.dep_centersX)))
         return self.vec_centersX, self.vec_centersY
 
         #return self.dep_centersX, self.dep_centersY
