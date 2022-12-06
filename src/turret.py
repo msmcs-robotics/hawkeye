4420c50299cc15100bfa3995d6c5003ce587cc9c
gndpwnd <gnelsondev@gmail.com>
2022-12-05 17:04:34 -0900

    attempt threading

--- src/turret.py
+++ src/turret.py
@@ -3,6 +3,7 @@ import time
 import os
 from serial import Serial
 from playsound import playsound
+import threading
 
 alarm_file = "../assets/owl.mp3"
 
@@ -15,10 +16,19 @@ elif os.name == 'posix':
 
 arduino = Serial(port='COM14', baudrate=baud_rate)
 
+
 def fire(x, y):
     if arduino.isOpen():
         arduino.write(bytes(f'({x},{y})', 'utf-8'))
         #print(f'{Fore.GREEN}({x},{y}){Fore.RESET}')
 
 def alarm():
-    playsound(alarm_file, False)
+    playsound(alarm_file)
+
+def run(x, y):
+    t1 = threading.Thread(target=fire, args=(x, y))
+    t2 = threading.Thread(target=alarm)
+    t1.start()
+    # check if alarm thread is alive
+    if not t2.is_alive():
+        t2.start()
